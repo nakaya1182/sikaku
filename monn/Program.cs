6d7b50c1fc4aee42b614c954033337f29f3f7e29
nakaya1182 <nakayakazuki1182@se-project.co.jp>
2019-10-31 11:50:24 +0900

    プロジェクト ファイルを追加します。

---
+++ monn/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace monn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = "十";
+            Console.Write("\n");
+            for (int hen = 25;hen>=0;hen-- )
+            {
+                int i = hen;
+                for (; i >= 0; i--)
+                {
+                    Console.Write("　");
+                }
+                i = 26-hen;
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(str);
+                    if(j!=i-1)
+                        Console.Write(str);
+
+                }
+                Console.Write("\n");
+            }
+            for(int hen = 24; hen >= 0; hen--){
+                Console.Write("　");
+                for(int i = 24 - hen; i >= 0; i--)
+                {
+                    Console.Write("　");
+                }
+
+                for (int i = hen; i >= 0; i--)
+                {
+                    Console.Write(str);
+                    if (i != hen )
+                        Console.Write(str);
+                }
+                Console.Write("\n");
+
+            }
+        }
+    }
+}
